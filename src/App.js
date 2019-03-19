4c3d499136be1ef14117fc34990c3b10a736d7e6
Nicholas Marcaccini <nicholas@Nicholass-MacBook-Pro.local>
2019-03-19 15:06:16 +0000

    first commit;

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import {jsx, css} from '@emotion/core';
+import {useState, useEffect} from 'react';
+
+const appCss = css`
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const counterCss = css`
+  font-weight: bold;
+  font-size: 40px;
+`;
+
+function useCounter(max, _isRunning) {
+  const [counter, setCounter] = useState(0);
+  const [isRunning, setIsRunning] = useState(_isRunning);
+
+  useEffect(() => {
+    if (isRunning) {
+      const timeout = setTimeout(() => {
+        if (counter === max) {
+          setIsRunning(false);
+        } else {
+          setCounter(counter + 1);
+        }
+      }, 50);
+
+      return () => clearTimeout(timeout) || console.log('clean');
+    }
+  }, [isRunning, counter]);
+
+  return counter;
+}
+
+function App() {
+  const [max, setMax] = useState(0);
+
+  return (
+    <div css={appCss}>
+      <input type="text" value={max} onChange={e => setMax(parseInt(e.target.value) || 0)} />
+      <Counter max={max} key={max}/>
+    </div>
+  );
+}
+
+function Counter({max}) {
+  const counter = useCounter(max, true);
+
+  return (
+    <div css={counterCss}>
+      {counter}
+    </div>
+  );
 }
 
 export default App;
